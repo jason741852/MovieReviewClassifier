722fc55c733288d09f114bc0eb13218fe466aa43
Jason <jason741852@gmail.com>
2017-07-21 14:20:49 -0700

    First commit

---
+++ model.py
@@ -0,0 +1,17 @@
+import nltk, re, pprint
+from nltk import word_tokenize
+from sklearn.naive_bayes import
+
+
+class ReviewModel():
+
+    def __init__(self, dataframe=None):
+        pass
+
+
+
+
+
+def main():
+    #Instanstiate the wrapper model
+    model = ReviewModel()

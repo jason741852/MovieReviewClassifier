c54d59859afa0a03645098cc9649054c4c0b3fb0
Jason <jason741852@gmail.com>
2017-07-24 11:58:34 -0700

    Add method to load raw text into pandas dataframe

--- model.py
+++ model.py
@@ -1,12 +1,48 @@
 import nltk, re, pprint
+import pandas as pd
 from nltk import word_tokenize
-from sklearn.naive_bayes import
+import glob
+
 
 
 class ReviewModel():
 
     def __init__(self, dataframe=None):
+        self.dataframe = dataframe;
         pass
+
+    def get_data(self):
+        column_names = ["msg"]
+        #with open('txt_sentoken/neg/cv000_29416.txt', 'r') as content_file:
+        #     df = content_file.read().replace("\n","")
+        #    df = pd.read_csv(content_file, names=column_names)
+        #    print (df)
+        i=0
+        list=[]
+        for filename in glob.glob("txt_sentoken/neg/*.txt"):
+            frame = pd.read_csv(filename, names=column_names)
+            list.append(frame)
+        df = pd.concat(list)
+        print (df)
+
+
+        return True
+
+    def parse_and_split_data(self, df):
+        pass
+
+    def engineer_features(self, df):
+        pass
+
+    def tokenizer(self):
+        file_content = open
+
+    def train(self, x, y):
+        pass
+
+    def predict(self, x):
+        pass
+
 
 
 
@@ -15,3 +51,9 @@ class ReviewModel():
 def main():
     #Instanstiate the wrapper model
     model = ReviewModel()
+
+    df = model.get_data()
+
+
+if __name__ == '__main__':
+    main()

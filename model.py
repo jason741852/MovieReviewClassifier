d93a89d68032b2ba4b10d7922473122d07654ee5
Jason <jason741852@gmail.com>
2017-07-24 19:40:33 -0700

    Change classfier to decision tree. Increase number of features to unlimited for better prediction. Build tree method

--- model.py
+++ model.py
@@ -4,9 +4,13 @@ from nltk import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.datasets import load_iris
+from sklearn import tree
 import random
 import glob
+import os
 
 GOOD=1
 BAD=0
@@ -16,7 +20,7 @@ BAD=0
 class ReviewModel():
 
     def __init__(self, dataframe=None):
-        self.model = MultinomialNB()
+        self.model = tree.DecisionTreeClassifier()
         self.dataframe = dataframe;
         pass
 
@@ -41,11 +45,12 @@ class ReviewModel():
 
     def parse_and_split_data(self, df):
         X = df['review']
-        self._vect = CountVectorizer(analyzer='word', stop_words='english', max_features = 3, ngram_range=(1, 1), binary=False)
+        self._vect = CountVectorizer(analyzer='word', stop_words='english', ngram_range=(1, 3), binary=False)
         mcl_transformed = self._vect.fit_transform(X)
+        print(mcl_transformed.shape[1])
         y = df["label"]
 
-        X_train, X_test, y_train, y_test = train_test_split(mcl_transformed, y, test_size=0.5, random_state=random.randint(1, 100))
+        X_train, X_test, y_train, y_test = train_test_split(mcl_transformed, y, test_size=0.8, random_state=10)
 
         self.X = X
         return (X_train, X_test, y_train, y_test)
@@ -63,6 +68,14 @@ class ReviewModel():
         print('recall: {}'.format(recall))
         print('fscore: {}'.format(fscore))
 
+    def build_tree_graph(self):
+        iris = load_iris()
+        self.model = self.model.fit(iris.data, iris.target)
+        tree.export_graphviz(self.model, out_file='tree.dot')
+        os.system("dot -Tpng tree.dot -o tree.png")
+        os.system("xdg-open tree.png")
+
+
 
 
 def main():
@@ -78,6 +91,8 @@ def main():
     pred = model.predict(X_test)
 
     model.report_scores(pred, y_test)
+
+    model.build_tree_graph()
 
 
 if __name__ == '__main__':
